7a2351eb3910632ccd72dc005c8b7d77622e8bda
Kevin Murphy <kevin@murp.us>
2022-07-02 02:18:09 -0400

    libxw: Add some basic "rendering" machinery
    Ultimately, I think we'll probably want to support some different
    renderers (maybe something graphical somehow?).  At the moment, tho,
    the `StringRenderer` will just dump everything into a single `str`.

--- libxw/src/libxw/puzzle.py
+++ libxw/src/libxw/puzzle.py
@@ -6,6 +6,7 @@ from typing import Sequence
 from typing import Tuple
 from typing import TypeAlias
 from typing import Union
+from typing import cast
 
 
 CellIndex: TypeAlias = Tuple[int, int]
@@ -41,9 +42,39 @@ class BlackSquare:
     pass
 
 
+_POSITIVE_INFINITY = float("inf")
+_NEGATIVE_INFINITY = float("-inf")
+
+
 @dataclass
 class Puzzle:
     BLACK_SQUARE = BlackSquare()
 
     cells: Mapping[CellIndex, Union[Cell, BlackSquare]]
     clues: Mapping[ClueDirection, Mapping[CellIndex, Clue]]
+
+    @property
+    def width(self) -> int:
+        # TODO: This is *slow*! We should store this data some other way.
+        xmin = _POSITIVE_INFINITY
+        xmax = _NEGATIVE_INFINITY
+        for x, _ in self.cells.keys():
+            xmin = min(x, xmin)
+            xmax = max(x, xmax)
+        dx = xmax - xmin + 1  # Offset by one because we include both endpoints.
+        if _NEGATIVE_INFINITY < x < _POSITIVE_INFINITY:
+            return cast(int, dx)
+        return 0  # There were no cells.
+
+    @property
+    def height(self) -> int:
+        # TODO: This is *slow*! We should store this data some other way.
+        ymin = _POSITIVE_INFINITY
+        ymax = _NEGATIVE_INFINITY
+        for _, y in self.cells.keys():
+            ymin = min(y, ymin)
+            ymax = max(y, ymax)
+        dx = ymax - ymin + 1  # Offset by one because we include both endpoints.
+        if _NEGATIVE_INFINITY < y < _POSITIVE_INFINITY:
+            return cast(int, dx)
+        return 0  # There were no cells.
